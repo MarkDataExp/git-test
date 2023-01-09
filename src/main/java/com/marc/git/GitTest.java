6a57e20fcb93469f88b948309d0cb879fda25b5c
marc <marc.ma@orientalent.se>
2023-01-09 22:47:27 +0100

    re

--- src/main/java/com/marc/git/GitTest.java
+++ src/main/java/com/marc/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello git~~ also~~ dev~ conflict~");
         System.out.println("hello git~~ also~~ dev~ push on GitHub~");
         System.out.println("hello git~~ also~~ dev~ pull from GitHub~");
+        System.out.println("hello git~~ also~~ dev~ pull from GitHub~re");
     }
 }
