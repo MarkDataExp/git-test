21fa84681a6576c47851a247017e40a9dfe3a2dd
marc <marc.ma@orientalent.se>
2023-01-09 22:02:14 +0100

    IDE Git Commit~ Again~ Also~ aa ~233 ~dev ~conflict

--- src/main/java/com/marc/git/GitTest.java
+++ src/main/java/com/marc/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git~~ also~~ 2");
         System.out.println("hello git~~ also~~ 2333");
         System.out.println("hello git~~ also~~ dev");
+        System.out.println("hello git~~ also~~ dev~ conflict~");
     }
 }
